457f3c2cd2635cc6f766ad69df8f4113b339c8e1
aleopfitness <product@aleop.io>
2025-07-24 22:00:55 +0200

    Initial commit

---
+++ api/create-checkout-session.js
@@ -0,0 +1,19 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  const { lineItems, memberId } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: lineItems,
+      mode: 'payment', // Ou 'subscription' si abonnement récurrent
+      success_url: `https://aleopplatform.webflow.io/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://aleopplatform.webflow.io/cancel`,
+      metadata: { memberId, licences: JSON.stringify(lineItems.map(item => item.description)) }, // Ajoute les détails des licences dans metadata
+    });
+    res.status(200).json({ sessionId: session.id });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
