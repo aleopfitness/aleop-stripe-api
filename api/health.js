acfe84966c6222c777c605147343dbc89ef2152c
aleopfitness <product@aleop.io>
2025-09-11 13:50:47 +0200

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,9 +1,5 @@
 // /api/health.js
-module.exports = async (req, res) => {
-  const envs = ['MEMBERSTACK_API_KEY','MS_WEBHOOK_SECRET','KV_REST_API_URL','KV_REST_API_TOKEN','FRONT_URL'];
-  res.status(200).json({
-    ok: true,
-    env: Object.fromEntries(envs.map(k => [k, !!process.env[k]])),
-    now: Date.now()
-  });
+module.exports = async (_req, res) => {
+  res.setHeader('Cache-Control','no-store');
+  res.status(200).json({ ok:true, ts: Date.now() });
 };
